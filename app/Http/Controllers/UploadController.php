1f5fa1f4c47fd13caa8ba5d025eaaa09e8c9e614
vin <vin@localhost.localdomain>
2020-07-07 08:18:27 -0500

    upload task

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Intervention\Image\ImageManager as Image;
+
+class UploadController extends Controller
+{
+    public function index()
+    {
+        return view('upload/index');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'file' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048'
+        ]);
+
+        if(!$request->file('file')) {
+            return redirect()->back();
+        }
+
+        $image = $request->file('file');
+        $input['imagename'] = time().'.'.$image->extension();
+
+        $img = new Image();
+
+        //50x50
+        $destinationPath = public_path('/uploads/thumbnail');
+        $imageFullPath   = $destinationPath.'/'.$input['imagename'];
+        $img->make($image->path())
+            ->resize(50,50)
+            ->save($imageFullPath);
+        
+        //10x10
+        $destinationPath = public_path('/uploads/thumbnail2');
+        $imageFullPath   = $destinationPath.'/'.$input['imagename'];
+        $img->make($image->path())
+            ->resize(100,100)
+            ->save($imageFullPath);
+
+        //original size
+        $destinationPath = public_path('/uploads/images');
+        $image->move($destinationPath, $input['imagename']);
+
+        return redirect()->back()
+            ->with(['flash_success_message' => 'Image Uploaded Successfully'])
+            ->with('imageName',$input['imagename']);
+
+    }
+}
--- resources/views/upload/index.blade.php
+++ resources/views/upload/index.blade.php
@@ -1,1 +1,101 @@
 <?php
+//dd( url("/uploads/images/" . Session::get('imageName')) );
+//http://127.0.0.1:8000/uploads/images/1594123630.jpeg
+?>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Upload Image</title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+</head>
+<body>
+
+<div class="container" style="padding-top: 200px;">
+
+    <div class="content">
+
+        <h1>Upload Form</h1><br><br><br>
+        <div class="row">
+
+            <div class="col-md-4">
+
+                <!-- Show alert messages -->
+                @if(session()->has('flash_success_message'))
+                    <div class="alert alert-success">
+                        {{ session()->get('flash_success_message') }}
+                    </div>
+                @endif
+
+                @if($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach($errors->all() as $error)
+                                <li>{{$error}}</li>
+                            @endforeach
+                        </ul>
+
+                    </div>
+
+                @endif
+
+                <form class="form-control" method="POST" action="{{ route('upload.store') }}" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group">
+                        <input type="file" name="file">
+                    </div>
+                    <div class="form-group">
+                        <input class="btn btn-info" type="submit" value="Upload" accept="image/*">
+                    </div>
+                </form>
+            </div>
+
+            <!--SHOW IMAGES-->
+            <div class="show-images col-md-8">
+                <div class="row">
+
+                    @if(!Session::has('imageName'))
+                        <div class="col-md-6 alert alert-info">
+                            No images Found
+                        </div>
+
+                    @else
+                        <div class="col-md-4 images-space">
+                            <strong>Original Image:</strong>
+                            <br/>
+                            <img src="{{ url("/uploads/images/" . Session::get('imageName')) }}" />
+                        </div>
+
+                        <div class="col-md-2 images-space">
+                            <strong>100 X 100:</strong>
+                            <br/>
+                            <img src="{{ url("/uploads/thumbnail2/" . Session::get('imageName')) }}" />
+                        </div>
+
+                        <div class="col-md-2 images-space">
+                            <strong>50 X 50:</strong>
+                            <br/>
+                            <img src="{{ url("/uploads/thumbnail/" . Session::get('imageName')) }}" />
+                        </div>
+                    @endif
+
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+</div>
+
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</body>
+</html>
